1ad1651186d97e204e037d6db3ce306a1ed1327e
Christian LeVesque <christian@christianlevesque.io>
2021-04-30 18:02:59 -0500

    saving work

--- src/App.php
+++ src/App.php
@@ -2,9 +2,11 @@
 
 namespace Crossview\Exphpress;
 
+use \Closure;
 use \InvalidArgumentException;
 use Crossview\Exphpress\Http\Request;
 use Crossview\Exphpress\Http\Response;
+use Crossview\Exphpress\Middleware\MiddlewareInterface;
 
 class App
 {
@@ -12,7 +14,21 @@ class App
 	private Router     $router;
 	private Request    $request;
 	private Response   $response;
-	private array      $messages;
+
+	/**
+	 * @var string[] App-level status and error messages
+	 */
+	private array $messages;
+
+	/**
+	 * @var MiddlewareInterface[] Middleware container
+	 */
+	private array $middleware = [];
+
+	/**
+	 * @var Closure[] Next Middleware container
+	 */
+	private array $nextMiddlewares = [];
 
 	private function __construct( string $domain )
 	{
@@ -28,7 +44,7 @@ class App
 		$this->response = new Response( $domain );
 	}
 
-	public function getInstance( string $domain ): App
+	public static function getInstance( string $domain ): App
 	{
 		if ( !isset( self::$instance ) )
 		{
@@ -36,6 +52,46 @@ class App
 		}
 
 		return self::$instance;
+	}
+
+	/**
+	 * Registers a middleware on the application
+	 *
+	 * @param MiddlewareInterface $middleware The middleware to register
+	 * @return $this
+	 */
+	public function register( MiddlewareInterface $middleware ): App
+	{
+		array_push( $this->middleware, $middleware );
+
+		return $this;
+	}
+
+	/**
+	 * Executes the middleware registered on the application
+	 */
+	public function execute(): void
+	{
+		$request  = $this->request;
+		$response = $this->response;
+
+		for ( $i = 0; $i < count( $this->middleware ); $i++ )
+		{
+			if ( array_key_exists( $i + 1, $this->middleware ) )
+			{
+				$next = function () use ( $i, &$request, &$response )
+				{
+					// TODO: figure out how to construct each next() (probably will have to traverse the middleware container backwards, construct each next and unpush it on an array, then traverse the array normally when calling the middleware
+					$this->middleware[ $i + 1 ]->handle( $request, $response );
+				};
+			} else
+			{
+				$next = function ()
+				{
+
+				};
+			}
+		}
 	}
 
 	/**
@@ -60,5 +116,13 @@ class App
 	public function getResponse(): Response
 	{
 		return $this->response;
+	}
+
+	/**
+	 * @return MiddlewareInterface[]
+	 */
+	public function getMiddleware(): array
+	{
+		return $this->middleware;
 	}
 }
