143c97805073da7590fbd9121afc68eb4bef2732
Christian LeVesque <christian@christianlevesque.io>
2021-04-30 21:51:13 -0500

    debugging

--- src/App.php
+++ src/App.php
@@ -99,7 +99,7 @@ class App
 		}
 
 //		$this->middleware[ 0 ]->handle( $request, $response, $nextMiddleware[ 0 ] );
-		json_encode($nextMiddleware);
+		echo json_encode($nextMiddleware);
 		//		for ( $i = 0; $i < count( $processedMiddleware ); $i++ )
 		//		{
 		//			$processedMiddleware[ $i ]->handle( $request, $response, $nextMiddleware[ $i ] );
