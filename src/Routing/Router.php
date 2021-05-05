921b500fa007bb681fdd9222f469bd08d98f92c9
Christian LeVesque <christian@christianlevesque.io>
2021-05-05 17:33:58 -0500

    commented out old code, not ready to scrap yet (saving for reference)

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -107,124 +107,124 @@ class Router
 	 *
 	 * @return array Returns the exploded URI. If the Route or requested URI is for the home directory ('/'), the array will be empty
 	 */
-	protected function explodeRoute( string $uri ): array
-	{
-		$hasQuestionMark = strpos( $uri, '?' );
-
-		if ( $hasQuestionMark )
-		{
-			$uri = substr( $uri, 0, $hasQuestionMark );
-		}
-
-		$lastCharacterSlash = $uri[ -1 ] === '/';
-
-		if ( $lastCharacterSlash )
-		{
-			$uri = substr( $uri, 0, -1 );
-		}
-
-		if ( strlen( $uri ) > 0 )
-		{
-			$uri = substr( $uri, 1 );
-		}
-
-		return explode( '/', $uri );
-	}
-
-	/**
-	 * Utility function to match two URIs
-	 *
-	 * Performs various tests to determine if two exploded URIs match. If the Route length has more elements than the requested URI, there is no match. If the requested URI has more elements than the Route and the last element of the Route is NOT an asterisk (*), there is no match. The rest of the tests are performed 1:1 on each element of the exploded URIs.
-	 *
-	 * If the current portion of the $route exists, but the corresponding portion of the $uri does not, it can't match.
-	 *
-	 * If The current portion of the $route is an asterisk (*), and the corresponding portion of $uri exists (we know it does because of the previous test), then it automatically matches the rest of the $uri.
-	 *
-	 * If The current portion of the $route is a parameter, and the corresponding portion of the $uri is a non-empty value, it may be a match.
-	 *
-	 * If all other 1:1 tests have not returned or continued, and the current portions don't match, then the $route and $uri don't match.
-	 *
-	 * @param array $route The exploded Route
-	 * @param array $uri   The exploded request URI
-	 *
-	 * @return array|bool Returns an array containing any parameters matched by the URI. If no parameters were matched but the URI and Route match, the array will be empty. If the URI and Route don't match, returns false.
-	 */
-	protected function matchRoute( array $route, array $uri )
-	{
-		$parameters  = [];
-		$routeLength = count( $route );
-		$uriLength   = count( $uri );
-
-		if ( $routeLength > $uriLength )
-		{
-			return false;
-		}
-
-		if ( $uriLength > $routeLength && $route[ $routeLength - 1 ] !== "*" )
-		{
-			return false;
-		}
-
-		for ( $i = 0; $i < count( $route ); $i++ )
-		{
-			if ( !isset( $uri[ $i ] ) )
-			{
-				return false;
-			}
-
-			if ( $route[ $i ] === "*" )
-			{
-				break;
-			}
-
-			if ( !empty( $route[ $i ] ) && $route[ $i ][ 0 ] === ":" && !empty( $uri[ $i ] ) )
-			{
-				$parameter                = substr( $route[ $i ], 1 );
-				$parameters[ $parameter ] = $uri[ $i ];
-				continue;
-			}
-
-			// if all other tests have not returned or continued, and the current portions don't match, the route and uri don't match
-			if ( $route[ $i ] !== $uri[ $i ] )
-			{
-				return false;
-			}
-		}
-
-		return $parameters;
-	}
-
-	/**
-	 * Public method to execute Route matches
-	 *
-	 * Calls all protected methods used to match a Route to the request URI.
-	 *
-	 * @param Request  $request
-	 * @param Response $response
-	 */
-	public function executeRouteMatch( Request $request, Response $response )
-	{
-		$this->uri = $this->explodeRoute( $_SERVER[ 'REQUEST_URI' ] );
-
-		foreach ( $this->routes as $route )
-		{
-			$explodedRoute = $this->explodeRoute( (string) $route );
-			$parameters    = $this->matchRoute( $explodedRoute, $this->uri );
-
-			// if a route doesn't match, $parameters will be false. if it does, $parameters will be an array containing any matched parameters in the URI, so go ahead and save data and break out of the loop if $parameters isn't explicitly false
-			if ( $parameters !== false )
-			{
-				$this->matchedRoute = $route;
-				$this->parameters   = !empty( $parameters )
-					? $parameters
-					: [];
-				break;
-			}
-		}
-
-		if ( isset( $this->matchedRoute ) )
-		{
-			$this->matchedRoute->execute( $request, $response );
-		}
-	}
+//	protected function explodeRoute( string $uri ): array
+//	{
+//		$hasQuestionMark = strpos( $uri, '?' );
+//
+//		if ( $hasQuestionMark )
+//		{
+//			$uri = substr( $uri, 0, $hasQuestionMark );
+//		}
+//
+//		$lastCharacterSlash = $uri[ -1 ] === '/';
+//
+//		if ( $lastCharacterSlash )
+//		{
+//			$uri = substr( $uri, 0, -1 );
+//		}
+//
+//		if ( strlen( $uri ) > 0 )
+//		{
+//			$uri = substr( $uri, 1 );
+//		}
+//
+//		return explode( '/', $uri );
+//	}
+//
+//	/**
+//	 * Utility function to match two URIs
+//	 *
+//	 * Performs various tests to determine if two exploded URIs match. If the Route length has more elements than the requested URI, there is no match. If the requested URI has more elements than the Route and the last element of the Route is NOT an asterisk (*), there is no match. The rest of the tests are performed 1:1 on each element of the exploded URIs.
+//	 *
+//	 * If the current portion of the $route exists, but the corresponding portion of the $uri does not, it can't match.
+//	 *
+//	 * If The current portion of the $route is an asterisk (*), and the corresponding portion of $uri exists (we know it does because of the previous test), then it automatically matches the rest of the $uri.
+//	 *
+//	 * If The current portion of the $route is a parameter, and the corresponding portion of the $uri is a non-empty value, it may be a match.
+//	 *
+//	 * If all other 1:1 tests have not returned or continued, and the current portions don't match, then the $route and $uri don't match.
+//	 *
+//	 * @param array $route The exploded Route
+//	 * @param array $uri   The exploded request URI
+//	 *
+//	 * @return array|bool Returns an array containing any parameters matched by the URI. If no parameters were matched but the URI and Route match, the array will be empty. If the URI and Route don't match, returns false.
+//	 */
+//	protected function matchRoute( array $route, array $uri )
+//	{
+//		$parameters  = [];
+//		$routeLength = count( $route );
+//		$uriLength   = count( $uri );
+//
+//		if ( $routeLength > $uriLength )
+//		{
+//			return false;
+//		}
+//
+//		if ( $uriLength > $routeLength && $route[ $routeLength - 1 ] !== "*" )
+//		{
+//			return false;
+//		}
+//
+//		for ( $i = 0; $i < count( $route ); $i++ )
+//		{
+//			if ( !isset( $uri[ $i ] ) )
+//			{
+//				return false;
+//			}
+//
+//			if ( $route[ $i ] === "*" )
+//			{
+//				break;
+//			}
+//
+//			if ( !empty( $route[ $i ] ) && $route[ $i ][ 0 ] === ":" && !empty( $uri[ $i ] ) )
+//			{
+//				$parameter                = substr( $route[ $i ], 1 );
+//				$parameters[ $parameter ] = $uri[ $i ];
+//				continue;
+//			}
+//
+//			// if all other tests have not returned or continued, and the current portions don't match, the route and uri don't match
+//			if ( $route[ $i ] !== $uri[ $i ] )
+//			{
+//				return false;
+//			}
+//		}
+//
+//		return $parameters;
+//	}
+//
+//	/**
+//	 * Public method to execute Route matches
+//	 *
+//	 * Calls all protected methods used to match a Route to the request URI.
+//	 *
+//	 * @param Request  $request
+//	 * @param Response $response
+//	 */
+//	public function executeRouteMatch( Request $request, Response $response )
+//	{
+//		$this->uri = $this->explodeRoute( $_SERVER[ 'REQUEST_URI' ] );
+//
+//		foreach ( $this->routes as $route )
+//		{
+//			$explodedRoute = $this->explodeRoute( (string) $route );
+//			$parameters    = $this->matchRoute( $explodedRoute, $this->uri );
+//
+//			// if a route doesn't match, $parameters will be false. if it does, $parameters will be an array containing any matched parameters in the URI, so go ahead and save data and break out of the loop if $parameters isn't explicitly false
+//			if ( $parameters !== false )
+//			{
+//				$this->matchedRoute = $route;
+//				$this->parameters   = !empty( $parameters )
+//					? $parameters
+//					: [];
+//				break;
+//			}
+//		}
+//
+//		if ( isset( $this->matchedRoute ) )
+//		{
+//			$this->matchedRoute->execute( $request, $response );
+//		}
+//	}
 }
