76adbd8b77a1ed59a117c996abea21d60f74c157
Christian LeVesque <christian@christianlevesque.io>
2021-05-06 16:02:12 -0500

    saving work on ErrorHandling middleware

---
+++ src/Middleware/ErrorHandling.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Crossview\Exphpress\Middleware;
+
+use Closure;
+use Exception;
+use Crossview\Exphpress\Exceptions\ExphpressException;
+use Crossview\Exphpress\Http\Request;
+use Crossview\Exphpress\Http\Response;
+
+class ErrorHandling implements Middleware
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function handle( Request $request, Response $response, Closure $next )
+	{
+		// TODO: This needs to not send
+		try {
+			$next();
+		} catch(Exception $e) {
+			ob_clean();
+			$error = [
+				'message' => $e->getMessage(),
+				'trace' => $e->getTraceAsString(),
+				'code' => $e->getCode()
+			];
+			$response->status(500)
+				->send(json_encode($error));
+		}
+	}
+}
