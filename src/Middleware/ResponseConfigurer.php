419a681360c0366564f368c4420199f1896aa44a
Christian LeVesque <christian@christianlevesque.io>
2021-05-07 16:09:13 -0500

    saving work

---
+++ src/Middleware/ResponseConfigurer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Crossview\Exphpress\Middleware;
+
+use Closure;
+use Crossview\Exphpress\Http\Request;
+use Crossview\Exphpress\Http\Response;
+use Crossview\Exphpress\Providers\CookieProvider;
+use Crossview\Exphpress\Providers\HeadersProvider;
+
+class ResponseConfigurer implements Middleware
+{
+	public function handle( Request $request, Response $response, Closure $next )
+	{
+		$this->configureProviders( $response );
+	}
+
+	protected function configureProviders( Response $response ): ResponseConfigurer
+	{
+		$response->setHeadersProvider( new HeadersProvider() );
+		$response->setCookieProvider( new CookieProvider() );
+
+		return $this;
+	}
+}
